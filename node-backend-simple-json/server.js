f1292e343ba7576cac3c6054f784d2b875a0728b
huffvii <huffvii@gmail.com>
2023-08-18 15:46:32 -0500

    adding coin flip work

---
+++ node-backend-simple-json/server.js
@@ -0,0 +1,66 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer((req, res) => {
+
+  const readWrite = (file, contentType) => {
+    fs.readFile(file, function(err, data) {
+      res.writeHead(200, {'Content-Type': contentType});
+      res.write(data);
+      res.end();
+    });
+  }
+
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+
+  switch (page) {
+    case '/':
+      readWrite('index.html', 'text/html')
+      break;
+    case '/otherpage':
+      readWrite('otherpage.html', 'text/html')
+      break;
+    case '/otherotherpage':
+      readWrite('otherotherpage.html', 'text/html')
+      break;
+    case '/api':
+      let flipResult = "type 'flip' in the input box"
+      if (params['student'] == 'flip') {
+        flipResult = Math.random() <= .5 ? "heads" : "tails"
+      }
+      res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: flipResult,
+
+        }
+        res.end(JSON.stringify(objToJson));
+      break;
+    case '/css/style.css':
+      fs.readFile('css/style.css', function(err, data) {
+        res.write(data);
+        res.end();
+      });
+      break;
+    case '/js/main.js':
+      readWrite('js/main.js', 'text/javascript')
+      break;
+    default:
+      figlet('404!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+      });
+      break;
+  }
+});
+
+server.listen(8000);
